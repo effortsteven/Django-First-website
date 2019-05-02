e5d7460e29b058e35d8fa46b69440999991ec41a
effortsteven <effortsteven@gmail.com>
2019-05-02 11:16:57 +0300

    html changed

--- techs/views.py
+++ techs/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+app_name = 'tech'
+
+
+# def index(request):
+#     return HttpResponse('<h1>my tech page </h1>')
 
 def index(request):
-    return HttpResponse('<h1>my tech page </h1>')
+    return render(request, 'techs/index.html')
